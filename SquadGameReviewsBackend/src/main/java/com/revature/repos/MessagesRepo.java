feb190405e8f2e5c7c90f9347e6d189bf2834cb8
Joshua <jw.ellison42@gmail.com>
2019-10-06 18:45:00 -0400

    added all things messages

--- SquadGameReviewsBackend/src/main/java/com/revature/repos/MessagesRepo.java
+++ SquadGameReviewsBackend/src/main/java/com/revature/repos/MessagesRepo.java
@@ -1,5 +1,11 @@
 package com.revature.repos;
 
-public class MessagesRepo {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.models.Messages;
+
+@Repository
+public interface MessagesRepo extends CrudRepository<Messages, Integer>{ 
 
 }
