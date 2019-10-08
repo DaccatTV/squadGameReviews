c7bbf8c470598f8c7cfc2581eb3d36c9b3a584e0
CoelDeLeon <coeldeleon@gmail.com>
2019-10-08 12:25:53 -0400

    put all components in component folder, ..service folder

--- VGReviewsSite/src/app/app-routing.module.ts
+++ VGReviewsSite/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { BrowsepageComponent } from './browsepage/browsepage.component';
-import { GamespageComponent } from './gamespage/gamespage.component'
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { BrowsepageComponent } from './component/browsepage/browsepage.component';
+import { GamespageComponent } from './component/gamespage/gamespage.component'
 
 
 const routes: Routes = [
