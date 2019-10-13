b473a917fb20cfa5f72438a268c061ec7008e822
CoelDeLeon <coeldeleon@gmail.com>
2019-10-13 14:10:06 -0400

    check if null ngif

--- VGReviewsSite/src/app/app.component.ts
+++ VGReviewsSite/src/app/app.component.ts
@@ -7,16 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   ngOnInit() {
+    this.getStatus();
   }
   title = 'VGReviewsSite';
-  currentAccount = JSON.parse(sessionStorage.getItem("userObj"));
+  //currentAccount = JSON.parse(sessionStorage.getItem("userObj"));
 
   getStatus(){
-    var temp = JSON.parse(sessionStorage.getItem("userObj"));
-    console.log("The account name is: " + temp.username);
-    console.log("The account status is: " + temp.status);
-    //console.log();
-    return temp.status;
+    var temp = sessionStorage.getItem("userObj");
+    if(temp!=null){
+
+    //console.log("The account name is: " + temp.username);
+    //console.log("The account status is: " + temp.status);
+    console.log(JSON.parse(temp).status);
+    return JSON.parse(temp).status;
+    }
+    return null;
   }
 
   refresh(){
